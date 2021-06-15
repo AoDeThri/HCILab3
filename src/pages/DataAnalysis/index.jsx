74af99033c7b7291f80898c21d7c269aac4035d5
AoDeThri <740950342@qq.com>
2021-06-15 08:57:37 +0800

    add echarts

--- src/pages/DataAnalysis/index.jsx
+++ src/pages/DataAnalysis/index.jsx
@@ -1,25 +1,60 @@
 import { PageContainer } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
-import styles from './index.less';
+import { useMount } from "react-use"
+import ReactEChartsCore from 'echarts-for-react';
+import * as echarts from 'echarts/core';
+import { 
+  BarChart,
+  LineChart,
+} from 'echarts/charts';
+import {
+  GridComponent,
+  TitleComponent,
+  TooltipComponent,
+
+} from 'echarts/components';
+import {
+  CanvasRenderer
+} from 'echarts/renderers';
+
+echarts.use(
+  [TitleComponent, TooltipComponent, GridComponent, BarChart, CanvasRenderer]
+);
+
 
 export default () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
+
+  useMount(() => {
+    
+  })
+
+  const options = {
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    legend: {
+      data:['销量']
+    },
+    xAxis: {
+      data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }]
+  }
+
   return (
-    <PageContainer content="这是一个新页面，从这里进行开发！" className={styles.main}>
-      <div
-        style={{
-          paddingTop: 100,
-          textAlign: 'center',
-        }}
-      >
-        <Spin spinning={loading} size="large" />
-      </div>
+    <PageContainer content="这是一个新页面，从这里进行开发！" >
+      <ReactEChartsCore
+        echarts={echarts}
+        option={options}
+        notMerge={true}
+        lazyUpdate={true}
+      />
     </PageContainer>
   );
 };
